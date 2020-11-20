b3ed2e31668b19258bb8912266b82fd174a1eec7
YuriiDmytryshyn <yuriidmytryshyn@gmail.com>
2020-11-20 13:50:36 +0200

    AngularAdminBlogUpgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminComponent } from './components/admin/admin.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { AdminCategoryComponent } from './components/admin/admin-category/admin-category.component';
+import { AdminProductsComponent } from './components/admin/admin-products/admin-products.component';
+import { AdminBlogComponent } from './components/admin/admin-blog/admin-blog.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'blog' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'blog/:id', component: BlogDetailsComponent },
+  {
+    path: 'admin', component: AdminComponent, children: [
+      { path: '', pathMatch: 'full', redirectTo: 'admin-blog' },
+      { path: 'admin-category', component: AdminCategoryComponent },
+      { path: 'admin-products', component: AdminProductsComponent },
+      { path: 'admin-blog', component: AdminBlogComponent },
+    ]
+  },
+  { path: '**', component: BlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { AdminCategoryComponent } from './components/admin/admin-category/admin-category.component';
+import { AdminProductsComponent } from './components/admin/admin-products/admin-products.component';
+import { AdminBlogComponent } from './components/admin/admin-blog/admin-blog.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    BlogComponent,
+    AdminCategoryComponent,
+    AdminProductsComponent,
+    AdminBlogComponent,
+    BlogDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
